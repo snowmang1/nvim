c20e8d62255ddf4aaff1be6c83b53e7d5671c953
Evan Drake <evanmdrake@icloud.com>
2022-07-15 21:31:23 -0700

    changed  quick start script to be more helpful to users

--- .quick_start.sh
+++ .quick_start.sh
@@ -2,13 +2,29 @@
 
 # make sure to do this first it will install packer.nvim for plugin management
 
+if [[ $1 == '-h' || '--help' ]]; then
+  echo "to run installation: "
+  echo "bash .quick_start.sh mac"
+  echo " "
+  echo "In the event you do not have a mac"
+  echo "this script will not work properly for you currently"
+  echo "you will need to manually install nvim-packer and the language servers"
+  echo " "
+  echo "This script uses git to install nvim-packer"
+  echo "if your system does not have git on it you will"
+  echo "need to download and install that first"
+  exit 0
+fi
+
+# path to nvim-packer
 PACKER=~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 if [[ -d $PACKER ]]; then
   echo packer already exists
 else
   git clone --depth 1 https://github.com/wbthomason/packer.nvim\
-   ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+    $PACKER
+   #~/.local/share/nvim/site/pack/packer/start/packer.nvim
 fi
 
 if [[ $1 == 'mac' ]]; then
