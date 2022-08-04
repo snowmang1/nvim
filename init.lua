73e5e3a2f896d6c5213c314b0b445b753db592a2
Evan Drake <evanmdrake@icloud.com>
2022-08-03 20:57:05 -0700

    configured vim dios to not interfear with lua line, configed lualine to appear only on one line

--- init.lua
+++ init.lua
@@ -10,3 +10,6 @@ require('options')
 vim.cmd [[ autocmd BufRead,BufNewFile *.org set filetype=org ]]
 
 vim.cmd.colorscheme "oxocarbon"
+vim.diagnostic.config({ virtual_text = false })
+vim.diagnostic.config({ virtual_lines = { only_current_line = true } })
+vim.keymap.set("", "<Leader>l", require("lsp_lines").toggle, { desc = "Toggle lsp_lines" })
