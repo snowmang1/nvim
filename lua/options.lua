7c450dfaf087485cce84dbab21476603b4619d6b
Evan Drake <31263887+snowmang1@users.noreply.github.com>
2022-08-09 08:40:51 -0700

    added fidget, and adjusted some keymaps and options (#35)

--- lua/options.lua
+++ lua/options.lua
@@ -2,8 +2,10 @@
 local o = vim.o				-- global
 local wo = vim.wo			-- window local
 -- local bo = vim.bo	-- buffer local
+local cmd = vim.cmd
 
 -- window local
 wo.number = true
 o.cursorline = true
 o.signcolumn = 'number'
+cmd('set mouse= ') -- disables mouse control
