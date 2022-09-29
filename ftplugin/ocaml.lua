1953b868bb1d3ca9cec6550ea7f49f23f3c73368
Evan Drake <evanmdrake@icloud.com>
2022-09-29 11:42:21 -0700

    added comment macros to ocaml

--- ftplugin/ocaml.lua
+++ ftplugin/ocaml.lua
@@ -4,3 +4,10 @@ local bo = vim.bo
 bo.expandtab = false
 bo.tabstop = 2
 bo.shiftwidth = 2
+
+	-- helpers
+local map = vim.api.nvim_set_keymap
+local nore_opts = { noremap = true, silent = true }
+
+map('i', '(*' , '(**)' , nore_opts)
+map('i', '(*<cr>' , '(*<cr><space>*<cr>*)<up>' , nore_opts)
