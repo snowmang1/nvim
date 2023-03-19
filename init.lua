52cdca4243e1533fe51da02ee23a2ba5a4d39ac7
Evan Drake <evanmdrake@icloud.com>
2023-03-19 16:21:45 -0400

    changed init to accomodate lazy.nvim
    Signed-off-by: Evan Drake <evanmdrake@icloud.com>

--- init.lua
+++ init.lua
@@ -1,11 +1,27 @@
 -- Evan Drake neovim config
 
+-- lazy lua bootstrap
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
 -- required files
-require('plugins')
-require('autocmds')
-require('mappings')
-require('options')
-require('user_cmd')
+require('config.autocmds')
+require('config.mappings')
+require('config.options')
+require('config.user_cmd')
+
+ -- load lazy.nvim
+require("lazy").setup("plugins")
 
 -- custom filetype command
 vim.cmd [[ autocmd BufRead,BufNewFile *.org set filetype=org  ]]
