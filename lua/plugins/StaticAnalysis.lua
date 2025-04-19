dbdbeb046553aebb55c023201ad7264a92dc8034
Evan Drake <evanmdrake@icloud.com>
2025-04-19 16:46:58 -0700

    new update to nvim config
    Signed-off-by: Evan Drake <evanmdrake@icloud.com>

--- lua/plugins/StaticAnalysis.lua
+++ lua/plugins/StaticAnalysis.lua
@@ -108,4 +108,22 @@ return {
 			require'mason'.setup(opts)
 		end,
 	},
+
+	{
+		'Julian/lean.nvim',
+		event = { 'BufReadPre *.lean', 'BufNewFile *.lean' },
+
+		dependencies = {
+			'neovim/nvim-lspconfig',
+			'nvim-lua/plenary.nvim',
+			-- you also will likely want nvim-cmp or some completion engine
+		},
+
+		-- see details below for full configuration options
+		opts = {
+			lsp = {},
+			mappings = true,
+		}
+	},
+
 }
